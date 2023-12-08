08c746c699c4428b8ab4c50336c2e1eb4e5f6558
Ian Gallardo <gallard3@gmail.com>
2023-12-07 20:26:55 -0500

    day 2 avatar component

---
+++ secret-santa/web/src/components/Avatar/Avatar.tsx
@@ -0,0 +1,61 @@
+import Icon from "../Icon/Icon"
+import { eyeClosed } from "../Icon/Icon.stories"
+import Indicator from "../Indicator/Indicator"
+
+interface AvatarProps {
+  avatar?: string
+  alt?: string
+  letter?: string
+  icon?: string
+  isSantaHatShowing?: boolean 
+  indicator?: 'success' | 'warning' | 'error' | null
+}
+
+const Avatar = ({ 
+  avatar, 
+  alt, 
+  letter, 
+  icon, 
+  isSantaHatShowing, 
+  indicator 
+}: AvatarProps) => {
+  return (
+    <div className="relative inline-block">
+      { avatar && (
+        <img 
+          src={avatar} 
+          alt={alt} 
+          className="h-18 w-18 rounded-full object-cover border-white border-avatar"
+        />
+      )}
+      
+      { letter && !avatar && (
+        <div className="center h-18 w-18 rounded-full object-cover border-white border-avatar bg-spanishGreen text-2xl font-bold uppercase text-white">
+          {letter}
+        </div>
+      )}
+
+      { icon && !avatar && !letter && (
+      <div className="center h-18 w-18 rounded-full object-cover border-vistaBlue border-avatar bg-spanishGreen text-2xl font-bold uppercase text-white">
+        <Icon id={icon} size={32} />
+      </div>
+      )}
+      
+      { isSantaHatShowing && (
+          <img src="/images/santa-hat.png" 
+          alt="Santa Hat" 
+          className="absolute -right-[15px] -top-[19px]" 
+          />
+      )}
+
+      { indicator && (
+          <div className="absolute bottom-0 right-0">
+            <Indicator status={indicator} />
+        </div>
+      )}
+
+    </div>
+  )
+}
+
+export default Avatar
