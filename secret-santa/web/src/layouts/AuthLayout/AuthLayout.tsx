75b49d1fffcfbbaeb4c66c81b9a87b1a21907c30
Ian Gallardo <gallard3@gmail.com>
2023-12-11 16:21:02 -0500

    day 6 slide up footer

--- secret-santa/web/src/layouts/AuthLayout/AuthLayout.tsx
+++ secret-santa/web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,13 +1,18 @@
+import Footer from "src/components/Footer/Footer"
+
 type AuthLayoutProps = {
   children?: React.ReactNode
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <main className=" min-h-screen min-w-screen bg-auth bg-silverTree dark:bg-nileBlue bg-[center_top] bg-no-repeat bg-[length:1440px_auto] min-[1440px]:bg-[length:100%_auto]">
-      <img src="/images/logo__secret-santa.svg" alt="Secret Santa" className="mx-auto mb-24 w-[460px] pt-24"/>
-      {children}
-    </main>
+    <>
+      <main className=" min-h-screen min-w-screen bg-auth bg-silverTree dark:bg-nileBlue bg-[center_top] bg-no-repeat bg-[length:1440px_auto] min-[1440px]:bg-[length:100%_auto]">
+        <img src="/images/logo__secret-santa.svg" alt="Secret Santa" className="mx-auto mb-24 w-[460px] pt-24" />
+        {children}
+      </main>
+      <Footer />
+    </>
   )
 }
 
